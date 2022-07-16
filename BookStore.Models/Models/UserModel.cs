f6ecd3a38113784ba970cf7b04bdfb3c3de7c216
Chirag Murari <84890563+Chirag-Murari@users.noreply.github.com>
2022-07-16 12:01:57 +0530

    Add files via upload

---
+++ BookStore.Models/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }        
+        public int Roleid { get; set; }
+    }
+}
