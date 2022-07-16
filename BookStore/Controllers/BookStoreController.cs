8cdb18fe2073c4b2a05b184effa85839f26534d2
Chirag Murari <84890563+Chirag-Murari@users.noreply.github.com>
2022-07-16 12:35:04 +0530

    Add files via upload

---
+++ BookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,55 @@
+﻿using BookStore.Models.Models;
+using BookStore.Models.ViewModels;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/public")]
+    [ApiController]
+    public class BookStoreController : ControllerBase
+    {
+        UserRepository _repository = new UserRepository();
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginModel model)
+        {
+            try
+            {
+                User user = _repository.Login(model);
+                if (user == null)
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "User not found");
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(RegisterModel model)
+        {
+            try
+            {
+                User user = _repository.Register(model);
+                if (user == null)
+                    return StatusCode(HttpStatusCode.BadRequest.GetHashCode(), "Bad request");
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+    }
+}
---
+++ BookStore/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using BookStore.Models.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserRepository _repository = new UserRepository();
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult GetUsers(int pageIndex = 1, int pageSize = 10, string keyword = "")
+        {
+            try
+            {
+                var users = _repository.GetUsers(pageIndex, pageSize, keyword);
+                if(users == null)
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Please provide correct information");
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), users);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+            
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            try
+            {
+                var user = _repository.GetUser(id);
+                if (user == null)
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Please provide correct information");
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateUser(UserModel model) 
+        {
+            try
+            {               
+                if (model != null) 
+                {
+                    var user = _repository.GetUser(model.Id);
+                    if (user == null)
+                        return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "User not found");
+
+                    user.Firstname = model.Firstname;
+                    user.Lastname = model.Lastname;
+                    user.Email = model.Email;
+
+                    var isSaved = _repository.UpdateUser(user);
+                    if (isSaved) 
+                    {
+                        return StatusCode(HttpStatusCode.OK.GetHashCode(), "User detail updated successfully");
+                    }
+                }
+
+                return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Please provide correct information");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public IActionResult DeleteUser(int id)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    var user = _repository.GetUser(id);
+                    if (user == null)
+                        return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "User not found");
+
+                    var isDeleted = _repository.DeleteUser(user);
+                    if (isDeleted)
+                    {
+                        return StatusCode(HttpStatusCode.OK.GetHashCode(), "User detail deleted successfully");
+                    }
+                }
+
+                return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Please provide correct information");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+    }
+}
